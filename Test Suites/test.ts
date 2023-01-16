149da20504812d181f9c8e3da78fa3d5ae2b31c4
2simo <2simo@DESKTOP-TGU1RFI.lan>
2023-01-16 15:54:04 +0200

    second

---
+++ Test Suites/test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fd24b6a4-b413-4a80-aa8a-b2aea20dd44f</testSuiteGuid>
+</TestSuiteEntity>
